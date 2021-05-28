0a8defc19de0c7f869f693117c17ae21e7cd82e4
xalphonseinbaraj <47241538+xalphonseinbaraj@users.noreply.github.com>
2021-05-28 18:47:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
-
-@app.route("/hi")
-def who():
-    return "Congratulations, this is another way to show HIII!!!!!"
-
-if __name__ == "__main__":
-    app.run()
